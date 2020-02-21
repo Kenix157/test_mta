22f3cbfde855810f748709d1f2264db199b34f52
Ilya Volkov <kenix157@gmail.com>
2020-02-21 23:04:25 +0300

    Update server.lua
    Исправлены опечатки

--- ui_test/src/server.lua
+++ ui_test/src/server.lua
@@ -10,7 +10,7 @@ addEventHandler( "onResourceStart", resourceRoot,
 	function()
 		connection = dbConnect ( "mysql", "dbname=dev_test;host=127.0.0.1;charset=utf8", "Kenix", "secret" );
 		
-		dbQuery( connection, 
+		dbExec( connection, 
 			"CREATE TABLE IF NOT EXISTS users ( \
 				id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,  \
 				name VARCHAR(32),  \
@@ -26,6 +26,8 @@ addEventHandler( "onPlayerRequestListUsers", root,
 		dbQuery( function( query, source )
 			local result = dbPoll( query, 0 );
 			
+			dbFree( query );
+			
 			triggerClientEvent( source, "onClientReciveUsers", source, result );
 		end, { source }, connection, "SELECT id, name, last_name, address FROM users" );
 	end
@@ -38,6 +40,8 @@ addEventHandler( "onPlayerAddUser", root,
 			
 			user.id = id;
 			
+			dbFree( query );
+			
 			triggerClientEvent( source, "onClientAddUser", source, user );
 		end, { source }, connection, "INSERT INTO users (name, last_name, address) VALUES ( ?, ?, ? )", user.name, user.last_name, user.address );
 	end
@@ -46,6 +50,8 @@ addEventHandler( "onPlayerAddUser", root,
 addEventHandler( "onPlayerRemoveUser", root,
 	function( user )
 		dbQuery( function( query, source )
+			dbFree( query );
+			
 			triggerClientEvent( source, "onClientRemoveUser", source, user );
 		end, { source }, connection, "DELETE FROM users WHERE id = ? LIMIT 1", user.id );
 	end
@@ -54,6 +60,8 @@ addEventHandler( "onPlayerRemoveUser", root,
 addEventHandler( "onPlayerEditUser", root,
 	function( user )
 		dbQuery( function( query, source )
+			dbFree( query );
+			
 			triggerClientEvent( source, "onClientEditUser", source, user );
 		end, { source }, connection, "UPDATE users SET name = ?, last_name = ?, address = ? WHERE id = ?", user.name, user.last_name, user.address, user.id );
 	end
