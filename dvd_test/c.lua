2b5b9eb306c432831440fd4503c2f959cb178082
Ilya Volkov <kenix157@gmail.com>
2020-02-26 12:06:58 +0300

    Исправлены недочеты

--- dvd_test/c.lua
+++ dvd_test/c.lua
@@ -1,5 +1,4 @@
 local getTickCount = getTickCount;
-local floor = math.floor;
 local random = math.random;
 local dxDrawImage = dxDrawImage;
 local dxDrawRectangle = dxDrawRectangle;
@@ -17,18 +16,22 @@ local height = 45;
 
 local texture;
 
-local speedX = 0.0001;
-local speedY = 0.0001;
+local speed = 0.1;
+
+local speedX = speed;
+local speedY = speed;
 
 local items = {
 	[ 1 ] = { x = 0, y = 0, speed_x = speedX, speed_y = speedY, color = 0xFFFFFFFF, min_pos_x = 0, 			max_pos_x = screenX / 2 };
 	[ 2 ] = { x = 0, y = 0, speed_x = speedX, speed_y = speedY, color = 0xFFFFFFFF, min_pos_x = screenX / 2, max_pos_x = screenX };
 };
 
-local function OnPreRender( dt )
-	dxDrawRectangle( 0, 0, screenX, screenY, tocolor( 0, 0, 0 ) );
+local prevTick;
+
+local function OnRender()
+	local tick = getTickCount();
 	
-	local dt = dt / 1000;
+	dxDrawRectangle( 0, 0, screenX, screenY, tocolor( 0, 0, 0 ) );
 	
 	for i, v in ipairs( items ) do
 		local posX = v.x;
@@ -39,14 +42,18 @@ local function OnPreRender( dt )
 		local minPosX = v.min_pos_x;
 		local maxPosX = v.max_pos_x;
 	
-		posX = posX + speedX * dt * getTickCount();
-		posY = posY + speedY * dt * getTickCount();
+		posX = posX + speedX * ( getTickCount() - prevTick );
+		posY = posY + speedY * ( getTickCount() - prevTick );
 		
 		local hit = false;
 		
 		if ( posX + width > maxPosX or posX < minPosX ) then
-			if ( posX < 0 ) then
-				posX = 0;
+			if ( posX < minPosX ) then
+				posX = minPosX;
+			end
+			
+			if ( posX + width > maxPosX ) then
+				posX = maxPosX - width;
 			end
 			
 			speedX = - speedX;
@@ -57,6 +64,10 @@ local function OnPreRender( dt )
 		if ( posY + height > screenY or posY < 0 ) then
 			if ( posY < 0 ) then
 				posY = 0;
+			end
+			
+			if ( posY + height > screenY ) then
+				posY = screenY - height;
 			end
 			
 			speedY = - speedY;
@@ -75,13 +86,15 @@ local function OnPreRender( dt )
 		v.color = color;
 		
 		dxDrawImage( 
-			floor( posX ), floor( posY ), 
+			posX, posY, 
 			width, height,
 			texture,
 			0, 0, 0,
 			color
 		);
 	end
+	
+	prevTick = tick;
 end
 
 addEventHandler( "onClientResourceStart", resourceRoot,
@@ -94,15 +107,19 @@ addCommandHandler( "toggle_anim",
 	function()
 		start = not start;
 		
+		showChat( not start );
+		
 		if ( start ) then
 			for i, v in ipairs( items ) do
 				v.x = v.min_pos_x + math.random( 150, 300 );
 				v.y = math.random( 150, 300 );
 			end
+			
+			prevTick = getTickCount();
 		
-			addEventHandler( "onClientPreRender", root, OnPreRender );
+			addEventHandler( "onClientRender", root, OnRender );
 		else
-			removeEventHandler( "onClientPreRender", root, OnPreRender );
+			removeEventHandler( "onClientRender", root, OnRender );
 		end
 	end
 );
